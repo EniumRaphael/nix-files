ecfe399666d7af018fcaa418e05b2f6f035e1569
Raphael <rparodi@student.42.fr>
2025-02-03 14:10:50 +0100

    feat: adding the home-manager configuration

--- flake.nix
+++ flake.nix
@@ -2,11 +2,15 @@
 	description = "NixOS Configuration";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 	};
 
-	outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+	outputs = { self, nixpkgs, flake-utils, home-manager,... }@inputs:
 	let
 		pkgs = import nixpkgs {
 			config.allowUnfree = true;
@@ -14,7 +18,9 @@
 	in {
 		nixosConfigurations."nixos-fix" = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
-			modules = [ ./hosts/fix/configuration.nix ];
+			modules = [
+				./hosts/fix/configuration.nix
+			];
 		};
 	};
 }
