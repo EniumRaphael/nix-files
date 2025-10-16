625094c61c38d4257d4563ae53e94e249b66bf34
Raphael <rparodi@student.42.fr>
2025-10-16 22:03:30 +0200

    feat(secrets): now using agenix to manage my secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -13,21 +13,4 @@ in
 {
   "mailjet-user.age".publicKeys = users ++ systems;
   "mailjet-pass.age".publicKeys = users ++ systems;
-  imports = [ inputs.agenix.nixosModules.default ];
-
-  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-
-  age.secrets."mailjet-user" = {
-    file = ../../secrets/mailjet-user.age;
-    owner = "root";
-    group = "root";
-    mode  = "0400";
-  };
-
-  age.secrets."mailjet-pass" = {
-    file = ../../secrets/mailjet-pass.age;
-    owner = "root";
-    group = "root";
-    mode  = "0400";
-  };
 }
