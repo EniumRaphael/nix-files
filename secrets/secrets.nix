da565d5b6cf8287f6c170cd7841b850e56817565
Raphael <rparodi@student.42.fr>
2025-10-19 21:12:49 +0200

    feat(secret/nextcloud): adding nextcloud secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -13,6 +13,8 @@ in
   "mailjet-user.age".publicKeys = users ++ systems;
   "mailjet-pass.age".publicKeys = users ++ systems;
   "authentik-env.age".publicKeys = users ++ systems;
+  "auth-nextcloud-id.age".publicKeys = users ++ systems;
+  "auth-nextcloud-secret.age".publicKeys = users ++ systems;
   "auth-grafana-id.age".publicKeys = users ++ systems;
   "auth-grafana-secret.age".publicKeys = users ++ systems;
 }
