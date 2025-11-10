c616d868b52ebc434ac6f96dc302039ab7b08353
Raphael <rparodi@student.42.fr>
2025-11-10 12:59:51 +0100

    feat(services/self_host): provied the jellyfin configuration

--- services/self_host/jellyfin.nix
+++ services/self_host/jellyfin.nix
@@ -9,6 +9,12 @@ let
 in
   {
   config = lib.mkIf cfg {
+    users = {
+      groups.datausers = { };
+      users = {
+        jellyfin.extraGroups = [ "datausers" ];
+      };
+    };
     services = {
       jellyfin = 
         {
@@ -17,17 +23,40 @@ in
           openFirewall = true;
         };
 
-      qbittorrent = {
-        enable = true;
-        dataDir = "/mnt/data/downloads";
-        webui.port = 8137;
-      };
+    qbittorrent = {
+      enable = true;
+      openFirewall = true;
+      user = "qbittorrent";
+      group = "datausers";
 
+      webuiPort = 8137;
+
+      serverConfig = {
+        Preferences = {
+          Downloads = {
+            SavePath = "/mnt/data/downloads";
+            TempPathEnabled = false;
+          };
+          General = {
+            Locale = "fr_FR";
+          };
+          WebUI = {
+            Username = "raphael";
+            Password_PBKDF2 = "@ByteArray(CmH/e4LVehCMTT2BUTVo5g==:VqhgnDIsg0owhZqINmi6O0Ac3tXgz6JYAkxB7sqSH18VPQ6R6Tz9jT2a6KXtld4wG6ld41nFXSst0UqRFTUTUw==)";
+          };
+        };
+      };
+    };
+      flaresolverr = {
+        enable = true;
+        openFirewall = true;
+        port = 8191;
+      };
       radarr = {
         enable = true;
         dataDir = "/var/lib/radarr";
         user = "radarr";
-        group = "media";
+        group = "datausers";
         openFirewall = true;
       };
 
@@ -38,6 +67,23 @@ in
       };
 
       bazarr.enable = true;
+
+      nginx.virtualHosts = {
+        "jellyfin.enium.eu" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:8096";
+          };
+        };
+        "radarr.enium.eu" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:7878";
+          };
+        };
+      };
     };
   };
 }
