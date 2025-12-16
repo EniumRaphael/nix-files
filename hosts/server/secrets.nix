9cf6067e95712327d76e1b2791dfa3b43df53c8b
Raphael <rparodi@student.42.fr>
2025-12-16 11:53:17 +0100

    feat(server/secrets): adding the kanidm secrets

--- hosts/server/secrets.nix
+++ hosts/server/secrets.nix
@@ -18,36 +18,11 @@
     mode  = "0400";
   };
 
-  age.secrets."authentik-env" = {
-    file = ../../secrets/authentik-env.age;
+  age.secrets."kanidm-pass" = {
+    file = ../../secrets/mailjet-pass.age;
     owner = "root";
     group = "root";
     mode  = "0400";
   };
 
-  age.secrets."auth-grafana-id" = {
-    file  = ../../secrets/auth-grafana-id.age;
-    owner = "root";
-    group = "grafana";
-    mode  = "0440";
-  };
-  age.secrets."auth-grafana-secret" = {
-    file  = ../../secrets/auth-grafana-secret.age;
-    owner = "root";
-    group = "grafana";
-    mode  = "0440";
-  };
-
-  age.secrets."auth-nextcloud-id" = {
-    file  = ../../secrets/auth-nextcloud-id.age;
-    owner = "root";
-    group = "nextcloud";
-    mode  = "0440";
-  };
-  age.secrets."auth-nextcloud-secret" = {
-    file  = ../../secrets/auth-nextcloud-secret.age;
-    owner = "root";
-    group = "nextcloud";
-    mode  = "0440";
-  };
 }
