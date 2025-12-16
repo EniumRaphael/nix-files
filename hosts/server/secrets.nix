e9903081fe4f68f5efde69942d19b88db5cbe8af
Raphael <rparodi@student.42.fr>
2025-12-16 15:12:44 +0100

    feat(server/secrets): update secrets on server side

--- hosts/server/secrets.nix
+++ hosts/server/secrets.nix
@@ -18,10 +18,31 @@
     mode  = "0400";
   };
 
-  age.secrets."kanidm-pass" = {
-    file = ../../secrets/mailjet-pass.age;
-    owner = "root";
-    group = "root";
+  age.secrets."nextcloud-admin-pass" = {
+    file = ../../secrets/nextcloud-admin-pass.age;
+    owner = "nextcloud";
+    group = "nextcloud";
+    mode  = "0400";
+  };
+
+  age.secrets."nextcloud-database" = {
+    file = ../../secrets/nextcloud-database.age;
+    owner = "nextcloud";
+    group = "nextcloud";
+    mode  = "0400";
+  };
+
+  age.secrets."kanidm-admin" = {
+    file = ../../secrets/kandim-admin.age;
+    owner = "kanidm";
+    group = "kanidm";
+    mode  = "0400";
+  };
+
+  age.secrets."kanidm-idmAdmin" = {
+    file = ../../secrets/kandim-idmAdmin.age;
+    owner = "kanidm";
+    group = "kanidm";
     mode  = "0400";
   };
 
