8456ea147c57acd5812257b43e04f45786018a09
Raphael <rparodi@student.42.fr>
2026-03-01 22:13:32 +0100

    feat(services/server): adding proxy setup for teamspeak redirection

--- services/server/teamspeak.nix
+++ services/server/teamspeak.nix
@@ -21,6 +21,13 @@ in
         locations."/" = {
           proxyPass = "http://127.0.0.1:9987";
           proxyWebsockets = true;
+          extraConfig = ''
+            proxy_ssl_verify off;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto https;
+          '';
         };
       };
     };
