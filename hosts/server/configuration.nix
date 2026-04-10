fe73c9ef1157d566a6d4f8705d5ff2a6ce117ab4
Raphael <rparodi@student.42.fr>
2026-04-11 00:23:59 +0200

    feat(hosts/server): adding the key for fix connection

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -8,6 +8,7 @@
 
 let
   sshKeyMac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIML4yVz1fhccwaTL0iHixkNkU5zUWU1rsit9u2TIIa5r raphael@raphaels-MacBook-Pro.local";
+  sshKeyFix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHqvUXdRS78wloWZnSqiOt63Gx0QRJdHxggrrepOd9+1 raphael@nixos-fix";
 in
 {
   imports = [
@@ -73,7 +74,7 @@ in
       marty = false;
       ada = false;
       music = false;
-      tempvoc = false;
+      tempvoc = true;
       ticket = false;
     };
   };
@@ -130,6 +131,7 @@ in
 
   users.users.raphael.openssh.authorizedKeys.keys = [
     sshKeyMac
+    sshKeyFix
   ];
   services = {
     seatd.enable = true;
