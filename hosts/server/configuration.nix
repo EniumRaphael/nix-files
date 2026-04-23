cf81cd758dd610d53ea3cf6b407e2ba43a4eb955
Raphael <rparodi@student.42.fr>
2026-04-24 00:10:40 +0200

    feat(hosts/server): now able to connect using my sshkey

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -8,6 +8,7 @@
 
 let
   sshKeyMac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIML4yVz1fhccwaTL0iHixkNkU5zUWU1rsit9u2TIIa5r raphael@raphaels-MacBook-Pro.local";
+  sshKeyFramework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlz8TaFYDbMiEmx3Pt5jEyHE169zImZTHTaP9IfhFCK raphael@nixos-fix";
   sshKeyFix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHqvUXdRS78wloWZnSqiOt63Gx0QRJdHxggrrepOd9+1 raphael@nixos-fix";
 in
 {
@@ -131,6 +132,7 @@ in
 
   users.users.raphael.openssh.authorizedKeys.keys = [
     sshKeyMac
+    sshKeyFramework
     sshKeyFix
   ];
   services = {
