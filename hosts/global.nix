abdeffd2ba40e792f2acd1dc0145ccc706d8a230
Raphael <rparodi@student.42.fr>
2026-04-25 17:17:41 +0200

    feat(host): adding the hyprland cachix

--- hosts/global.nix
+++ hosts/global.nix
@@ -60,6 +60,9 @@
     };
     settings = {
       download-buffer-size = 268435456;
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       experimental-features = [
         "nix-command"
         "flakes"
