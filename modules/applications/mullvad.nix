d2b0698ea3c83fc945797a1b3365e49280ece2f3
Raphael <rparodi@student.42.fr>
2026-05-06 14:36:00 +0200

    feat(modules/applications): adding the applications modules

---
+++ modules/applications/mullvad.nix
@@ -0,0 +1,27 @@
+{
+config,
+pkgs,
+lib,
+...
+}:
+
+let
+  cfg = config.applications.mullvad;
+  mullvad-autostart = pkgs.makeAutostartItem {
+    name = "mullvad-vpn";
+    package = pkgs.mullvad-vpn;
+  };
+in
+  {
+  config = lib.mkIf cfg {
+    environment.systemPackages = [
+      mullvad-autostart
+    ];
+    services = {
+      mullvad-vpn = {
+        enable = true;
+        package = pkgs.mullvad-vpn;
+      };
+    };
+  };
+}
