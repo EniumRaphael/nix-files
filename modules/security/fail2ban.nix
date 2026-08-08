b4712b747211403b6bde85f5019d06d99d3a6bb9
Raphael Parodi <raphael@parodi.pro>
2026-08-09 00:38:14 +0200

    fix(security[fail2ban]): Whitelist all internal IP

--- modules/security/fail2ban.nix
+++ modules/security/fail2ban.nix
@@ -14,7 +14,10 @@ in
       enable = true;
       maxretry = 3;
       ignoreIP = [
-        "192.168.0.0/24"
+        "127.0.0.1/8"
+        "192.168.0.0/16"
+        "10.0.0.0/8"
+        "172.16.0.0/12"
       ];
       bantime = "24h";
       bantime-increment = {
