8f9cb6d020e2fcef2e0e6fe33da623bab083a406
Raphael Parodi <raphael@parodi.pro>
2026-08-15 19:39:28 +0200

    feat(self_host[immich]): enable the oicd connection using kanidm

--- services/self_host/immich.nix
+++ services/self_host/immich.nix
@@ -6,15 +6,28 @@
 }:
 
 let
+  immichLogo = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/immich-app/immich/ffc83eae36034fe75525a0abd6c5df351184cd91/design/immich-logo.svg";
+    name = "immich.svg";
+    sha256 = "19hik27zb446syvzxpy0iw7p38mxfd0j433c60a4k1879mqfz9fz";
+  };
   cfg = config.service.selfhost.immich;
 in
 {
   config = lib.mkIf cfg {
-    age.secrets."immich-mail-password" = {
-      file = ../../secrets/immich-mail-password.age;
-      owner = "immich";
-      group = "immich";
-      mode = "0400";
+    age.secrets = {
+      "immich-mail-password" = {
+        file = ../../secrets/immich-mail-password.age;
+        owner = "immich";
+        group = "immich";
+        mode = "0400";
+      };
+      "immich-oidc-secret" = {
+        file = ../../secrets/immich-oidc-secret.age;
+        owner = "kanidm";
+        group = "immich";
+        mode = "0440";
+      };
     };
     networking.firewall.allowedTCPPorts = [
       80
@@ -107,6 +120,54 @@ in
               password._secret = config.age.secrets.immich-mail-password.path;
               port = 465;
               secure = true;
+            };
+          };
+          oauth = {
+            enabled = true;
+            issuerUrl = "https://auth.enium.eu/oauth2/openid/immich";
+            clientId = "immich";
+            clientSecret._secret = config.age.secrets.immich-oidc-secret.path;
+            scope = "openid email profile";
+            signingAlgorithm = "ES256";
+            buttonText = "Se connecter avec Enium";
+            autoRegister = true;
+            autoLaunch = true;
+            mobileOverrideEnabled = true;
+            mobileRedirectUri = "app.immich:///oauth-callback";
+            storageLabelClaim = "preferred_username";
+          };
+        };
+      };
+
+      kanidm.provision.systems.oauth2.immich = {
+        present = true;
+        displayName = "Immich";
+        imageFile = immichLogo;
+        originUrl = [
+          "https://immich.enium.eu"
+          "app.immich:///oauth-callback"
+        ];
+        originLanding = "https://immich.enium.eu/auth/login";
+        basicSecretFile = config.age.secrets.immich-oidc-secret.path;
+        public = false;
+        enableLocalhostRedirects = false;
+        allowInsecureClientDisablePkce = false;
+        preferShortUsername = true;
+        scopeMaps = {
+          immich_users = [
+            "email"
+            "openid"
+            "profile"
+            "groups"
+          ];
+        };
+        claimMaps = {
+          groups = {
+            joinType = "array";
+            valuesByGroup = {
+              immich_users = [
+                "immich_users"
+              ];
             };
           };
         };
