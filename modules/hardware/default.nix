531137308545b84219cf3d00d23bbacdc15f89df
Raphael Parodi <raphael@parodi.pro>
2026-09-12 11:41:56 +0200

    feat(hardware): enable the supported local used

--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -105,6 +105,11 @@ in
 
     time.timeZone = "Europe/Paris";
     i18n = {
+      supportedLocales = [
+        "fr_FR.UTF-8/UTF-8"
+        "en_US.UTF-8/UTF-8"
+        "en_GB.UTF-8/UTF-8"
+      ];
       defaultLocale = "en_US.UTF-8";
       extraLocaleSettings = {
         LC_ADDRESS = "fr_FR.UTF-8";
